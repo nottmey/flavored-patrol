0327d84caf39827f8365dce8a17a45c9c15a76f0
Malte Nottmeyer <nottmey@users.noreply.github.com>
2025-06-02 14:06:20 +0200

    chore: android tutorial

---
+++ integration_test/example_test.dart
@@ -0,0 +1,25 @@
+import 'package:flavored_patrol/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+void main() {
+  patrolTest('counter state is the same after going to home and going back', (
+    $,
+  ) async {
+    await $.pumpWidgetAndSettle(const MyApp());
+
+    await $(FloatingActionButton).tap();
+    expect($(#counterText).text, '1');
+
+    await $.native.pressHome();
+    await $.native.pressDoubleRecentApps();
+
+    expect($(#counterText).text, '1');
+    await $(FloatingActionButton).tap();
+    expect($(#counterText).text, '2');
+
+    await $.native.openNotifications();
+    await $.native.pressBack();
+  });
+}
