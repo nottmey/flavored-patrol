c8ed175a4802c5511f91935d6f6f5c6e15dd61b3
Malte Nottmeyer <nottmey@users.noreply.github.com>
2025-06-04 22:14:26 +0200

    feat: add ios testlab script

---
+++ run_ios_testlab.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+patrol build ios --target integration_test/example_test.dart --release
+
+pushd build/ios_integ/Build/Products
+zip -r ios_tests.zip Release-iphoneos Runner_iphoneos18.5-arm64.xctestrun
+popd
+
+# use `gcloud firebase test ios models list` to see available models & versions
+gcloud firebase test ios run \
+	--test build/ios_integ/Build/Products/ios_tests.zip \
+	--device model=ipad10,version=16.6,locale=en_US,orientation=portrait \
+	--results-bucket="patrol-runs"
